7094c41ef7dfd71cf53aa67700bc7a3461fafa4d
Chester Rosario <chester-codev@github.com>
2025-04-01 21:14:15 +1100

    Update group routes

---
+++ app-modules/group/routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\Group\Http\Controllers\GroupController;
+
+Route::get('/groups', [GroupController::class, 'index'])->name('groups.index');
+Route::post('/groups', [GroupController::class, 'store'])->name('groups.store');
+Route::put('/groups/{group}', [GroupController::class, 'update'])->name('groups.update');
+Route::delete('/groups/{group}', [GroupController::class, 'destroy'])->name('groups.destroy');
