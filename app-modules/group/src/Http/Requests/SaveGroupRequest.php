3e3cf5186aabdd7b497377ee9dea458fb278d6b2
Chester Rosario <chester-codev@github.com>
2025-04-01 21:13:56 +1100

    Add SaveGroupRequest

---
+++ app-modules/group/src/Http/Requests/SaveGroupRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modules\Group\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Modules\Group\Models\Group;
+
+class SaveGroupRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'name' => 'required',
+        ];
+    }
+
+    public function handle(Group $group): Group
+    {
+        $group->fill(['name' => $this->name])->save();
+        return $group;
+    }
+}
