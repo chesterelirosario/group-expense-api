0be470e74c6ff3986f23d5e38c69f6db51e7e462
Chester Rosario <chester-codev@github.com>
2025-04-02 16:56:27 +1100

    Update migrations

--- app-modules/group/database/migrations/2025_04_01_062211_create_groups_table.php
+++ app-modules/group/database/migrations/2025_04_01_062211_create_groups_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('groups', function (Blueprint $table) {
-            $table->uuid()->primary();
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->timestamps();
         });
