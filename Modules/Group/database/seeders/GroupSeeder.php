ba85a0b145adcb8939658f58a449640b729e375b
Chester Rosario <chester-codev@github.com>
2025-04-03 21:44:52 +1100

    Improve seeder

--- Modules/Group/database/seeders/GroupSeeder.php
+++ Modules/Group/database/seeders/GroupSeeder.php
@@ -3,12 +3,17 @@
 namespace Modules\Group\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Modules\Group\Events\GroupCreated;
 use Modules\Group\Models\Group;
 
 class GroupSeeder extends Seeder
 {
     public function run(): void
     {
-        Group::factory()->count(10)->create();
+        $groups = Group::factory()->count(10)->create();
+
+        foreach ($groups as $group) {
+            event(new GroupCreated($group->id, $group->owner_id));
+        }
     }
 }
--- Modules/Membership/database/seeders/MembershipSeeder.php
+++ Modules/Membership/database/seeders/MembershipSeeder.php
@@ -3,12 +3,23 @@
 namespace Modules\Membership\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Modules\Group\Models\Group;
+use Modules\Membership\Enums\Role;
 use Modules\Membership\Models\Membership;
 
 class MembershipSeeder extends Seeder
 {
     public function run(): void
     {
-        Membership::factory()->count(10)->create();
+        $groups = Group::all();
+
+        foreach ($groups as $group) {
+            Membership::factory()
+                ->count(rand(1, 5))
+                ->create([
+                    'group_id' => $group->id,
+                    'role' => Role::Member,
+                ]);
+        }
     }
 }
